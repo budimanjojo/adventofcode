d3914d7e6228c7a8a2563120d75adee41aad1a84
budimanjojo <budimanjojo@gmail.com>
2022-12-09 11:46:58 +0700

    fix(08): code cleanup

--- 2022/08/main.go
+++ 2022/08/main.go
@@ -16,32 +16,24 @@ func main() {
 	lines := strings.Split(string(f), "\n")
 	lines = lines[:len(lines)-1]
 	edges := len(lines)*2 + len(lines[0])*2 - 4
-	visible := edges
-	score := 0
+	part1Answer := edges
+	part2Answer := 0
 
 	for row := 1; row < len(lines)-1; row++ {
 		for col := 1; col < len(lines[row])-1; col++ {
 			num := getSize(lines[row][col])
-			if isVisible(num, row, col, lines) {
-				visible++
+			curScore, yes := checkAround(num, row, col, lines)
+			if yes {
+				part1Answer++
 			}
-			if getScore(num, row, col, lines) > score {
-				score = getScore(num, row, col, lines)
+			if curScore > part2Answer {
+				part2Answer = curScore
 			}
 		}
 	}
-	fmt.Println(visible)
-	fmt.Println(score)
-}
 
-func parseTreeSlice(s string) []int {
-	ss := strings.Split(s, "")
-	si := []int{}
-	for i := range ss {
-		in, _ := strconv.Atoi(ss[i])
-		si = append(si, in)
-	}
-	return si
+	fmt.Println(part1Answer)
+	fmt.Println(part2Answer)
 }
 
 func getSize(b byte) int {
@@ -49,87 +41,70 @@ func getSize(b byte) int {
 	return size
 }
 
-func isVisibleLeft(num, row, col int, lines []string) bool {
+func checkLeft(num, row, col int, lines []string) (int, bool) {
+	score := 0
+	visible := true
 	for i := col - 1; i >= 0; i-- {
 		if num <= getSize(lines[row][i]) {
-			return false
+			score++
+			visible = false
+			break
+		} else {
+			score++
 		}
 	}
-	return true
+	return score, visible
 }
 
-func isVisibleRight(num, row, col int, lines []string) bool {
+func checkRight(num, row, col int, lines []string) (int, bool) {
+	score := 0
+	visible := true
 	for i := col + 1; i < len(lines[row]); i++ {
 		if num <= getSize(lines[row][i]) {
-			return false
+			score++
+			visible = false
+			break
+		} else {
+			score++
 		}
 	}
-	return true
+	return score, visible
 }
 
-func isVisibleDown(num, row, col int, lines []string) bool {
+func checkDown(num, row, col int, lines []string) (int, bool) {
+	score := 0
+	visible := true
 	for i := row + 1; i < len(lines); i++ {
 		if num <= getSize(lines[i][col]) {
-			return false
+			score++
+			visible = false
+			break
+		} else {
+			score++
 		}
 	}
-	return true
+	return score, visible
 }
 
-func isVisibleTop(num, row, col int, lines []string) bool {
+func checkUp(num, row, col int, lines []string) (int, bool) {
+	score := 0
+	visible := true
 	for i := row - 1; i >= 0; i-- {
 		if num <= getSize(lines[i][col]) {
-			return false
+			score++
+			visible = false
+			break
+		} else {
+			score++
 		}
 	}
-	return true
+	return score, visible
 }
 
-func isVisible(num, row, col int, lines []string) bool {
-	top := isVisibleTop(num, row, col, lines)
-	bot := isVisibleDown(num, row, col, lines)
-	left := isVisibleLeft(num, row, col, lines)
-	right := isVisibleRight(num, row, col, lines)
-	return top || bot || left || right
-}
-
-func getScore(num, row, col int, lines []string) int {
-	var left, right, up, down int
-	for i := col - 1; i >= 0; i-- {
-		if num <= getSize(lines[row][i]) {
-			left++
-			break
-		} else {
-			left++
-		}
-	}
-	for i := col + 1; i < len(lines[row]); i++ {
-		if num <= getSize(lines[row][i]) {
-			right++
-			break
-		} else {
-			right++
-		}
-	}
-	for i := row + 1; i < len(lines); i++ {
-		if num <= getSize(lines[i][col]) {
-			down++
-			break
-		} else {
-			down++
-		}
-	}
-	for i := row - 1; i >= 0; i-- {
-		if num <= getSize(lines[i][col]) {
-			up++
-			break
-		} else {
-			up++
-		}
-	}
-	if left == 0 { left++ }
-	if right == 0 { right++ }
-	if down == 0 { down++ }
-	if up == 0 { up++ }
-	return left * right * down * up
+func checkAround(num, row, col int, lines []string) (int, bool) {
+	us, up := checkUp(num, row, col, lines)
+	ds, down := checkDown(num, row, col, lines)
+	ls, left := checkLeft(num, row, col, lines)
+	rs, right := checkRight(num, row, col, lines)
+	return ls * rs * ds * us, up || down || left || right
 }
